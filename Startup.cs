18b7a67bbf5e3132c090f6ec2e9af7b316044fa3
chalameg <chalamegersa5@gmail.com>
2020-01-19 18:19:07 +0300

    initial

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+//chala
 namespace DiscussTechPanel
 {
     public class Startup
